351e9229cebba76ed402f4c180257334013a4add
Oxana Odegova <oxyapple@yandex.ru>
2020-12-22 09:30:25 +0100

    add seach component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
-  {
-      title: 'What is React?',
-      content: 'React is framework'
-  },
-  {
-      title: 'Why to learn React?',
-      content: 'React is super'
-  },
-  {
-      title: 'What do you need to learn React?',
-      content: 'Time'
-  }
-]
+  {title: 'What is React?', content: 'React is framework'},
+  {title: 'Why to learn React?', content: 'React is super'},
+  {title: 'What do you need to learn React?', content: 'Time'}
+];
 
 export default () => {
   return (
     <div>
       <Accordion items={items} />
+      <Search />
     </div>
   );
-}
-
-
+};
