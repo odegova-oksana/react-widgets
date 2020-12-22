11dc0ccd32a02559ac60b9287dda18ef1dbc639a
Oxana Odegova <oxyapple@yandex.ru>
2020-12-22 09:01:47 +0100

    accordio fix

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -4,17 +4,19 @@ const Accordion = ({items}) => {
   const [activeIndex, setActiveIndex] = useState (null);
 
   const onTitleClicked = index => {
-    setActiveIndex(index);
+    setActiveIndex (index);
   };
 
   const renderedItems = items.map ((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClicked (index)}>
+        <div className={"title " +  active} onClick={() => onTitleClicked (index)}>
           <i className="dropdown icon" />
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>
             {item.content}
           </p>
@@ -26,7 +28,6 @@ const Accordion = ({items}) => {
   return (
     <div className="ui styled accordion">
       {renderedItems}
-      <h1>{activeIndex}</h1>
     </div>
   );
 };
