d4b0e08003bace79e0982eab0896b361004d681a
Oxana Odegova <oxyapple@yandex.ru>
2020-12-23 17:31:20 +0100

    dropdown start, add

---
+++ src/components/Dropdown.js
@@ -0,0 +1,35 @@
+import {render} from '@testing-library/react';
+import React from 'react';
+
+const Dropdown = ({options, selected, onSelectedChange}) => {
+  const renderedOptions = options.map (option => {
+    if (option.value === selected.value) {
+      return null;
+    }
+
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange (option)}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a colour</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon" />
+          <div className="text">{selected.label}</div>
+          <div className="menu visible transition">{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
