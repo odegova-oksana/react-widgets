0b25697e2c0ccce112856531e5cc9d5cb74fe7b3
Oxana Odegova <oxyapple@yandex.ru>
2020-12-23 17:31:04 +0100

    dropdown start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {title: 'What is React?', content: 'React is framework'},
   {title: 'Why to learn React?', content: 'React is super'},
-  {title: 'What do you need to learn React?', content: 'Time'}
+  {title: 'What do you need to learn React?', content: 'Time'},
+];
+
+const options = [
+  {label: 'Red', value: 'red'},
+  {label: 'Green', value: 'green'},
+  {label: 'Blue', value: 'blue'},
 ];
 
 export default () => {
+  const [selected, setSelected] = useState (options[0]);
+
   return (
     <div>
       <Accordion items={items} />
       <Search />
+      <Dropdown
+        onSelectedChange={setSelected}
+        selected={selected}
+        options={options}
+      />
     </div>
   );
 };
