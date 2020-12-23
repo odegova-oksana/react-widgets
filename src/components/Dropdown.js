4d8146c7c46baafb31ba2209a4da6dce9a75ee7c
Oxana Odegova <oxyapple@yandex.ru>
2020-12-23 18:10:29 +0100

    check event bubbling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,20 @@
 import {render} from '@testing-library/react';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
+  const [open, setOpen] = useState (false);
+
+  useEffect (() => {
+    document.body.addEventListener (
+      'click',
+      () => {
+        console.log ('BODY CLICK!');
+        setOpen (false);
+      },
+      {capture: true}
+    );
+  }, []);
+
   const renderedOptions = options.map (option => {
     if (option.value === selected.value) {
       return null;
@@ -11,7 +24,10 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
       <div
         key={option.value}
         className="item"
-        onClick={() => onSelectedChange (option)}
+        onClick={() => {
+          console.log ('ITEM CLICK!');
+          onSelectedChange (option);
+        }}
       >
         {option.label}
       </div>
@@ -22,10 +38,18 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a colour</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => {
+            console.log ('LABEL CLICK!');
+            setOpen (!open);
+          }}
+          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+        >
           <i className="dropdown icon" />
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
