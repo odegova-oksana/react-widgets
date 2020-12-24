ab7a592718623bffcd379ab70857ae3790e410d7
Oxana Odegova <oxyapple@yandex.ru>
2020-12-24 14:25:49 +0100

    translate start + some refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
-import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import DropdownPanel from './components/DropdownPanel';
 
 const items = [
   {title: 'What is React?', content: 'React is framework'},
@@ -9,30 +10,13 @@ const items = [
   {title: 'What do you need to learn React?', content: 'Time'},
 ];
 
-const options = [
-  {label: 'Red', value: 'red'},
-  {label: 'Green', value: 'green'},
-  {label: 'Blue', value: 'blue'},
-];
-
 export default () => {
-  const [selected, setSelected] = useState (options[0]);
-  const [showDropdown, setShowDropdown] = useState (true);
-
   return (
     <div>
       <Accordion items={items} />
       <Search />
-      <button onClick={() => setShowDropdown (!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown
-        ? <Dropdown
-            onSelectedChange={setSelected}
-            selected={selected}
-            options={options}
-          />
-        : null}
+      <DropdownPanel />
+      <Translate />
     </div>
   );
 };
